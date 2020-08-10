428a0d2d7d645013c81d4078fd6d21ee0796fefa
Jakeone Im <jakeoneijk@gmail.com>
2020-08-11 02:15:16 +0900

    try to make proper hopsize

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import numpy as np
 
+def stft(filePath,samplingRate,numFft,hopLength,winLength):
+    x, fs = librosa.load(filePath,sr=samplingRate)
+    return librosa.core.stft(x,n_fft=numFft, hop_length=hopLength, win_length=winLength,window='hann')
 
 test_file_path = './Data/mirex05TrainFiles/train01.wav'
-x, fs = librosa.load(test_file_path,  sr=44100)
-X = librosa.core.stft(x,n_fft=1024, hop_length=512, win_length=1024)
-x_shape = X.shape
+X = stft(test_file_path,44100,1024,944,1024)
+
+test_pitch_path = './Data/labrosa_pitch/pitch_train01.wav.txt'
+test_pitch = np.loadtxt('./Data/labrosa_pitch/pitch_train01.wav.txt', delimiter=" ")
+
 S = librosa.amplitude_to_db(abs(X))
 plt.figure(figsize=(15,5))
 librosa.display.specshow(S, sr=fs, hop_length=441, x_axis='time', y_axis='log')
